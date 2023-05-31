71b65d0a177d3a1345ec961120e6a203d5977de3
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2023-05-31 14:29:02 +0330

    Update main.py

--- test1/main.py
+++ test1/main.py
@@ -2,53 +2,25 @@ from multiprocessing import Process
 import os
 from flask import Request
 from fastapi import FastAPI
-import uvicorn
+from localpackage.api_manager import APIManager
+from localpackage.types import APIConfig
 
+API_STARTED_NAME = 'API_STARTED'
+os.environ[API_STARTED_NAME] = '0'
 
-app = FastAPI()
-start = False
-fapi_prcss = None 
+api = APIManager(APIConfig(name='testAPI'),FastAPI())
+app = api.as_app()
 
-@app.get('/')
+@app.get('/ping')
 def get_hello():
     """some example function"""
-    return {'hello':'world'}
-
-def start_fastapi():
-    print('fast api starting ...')
-    global start
-    if not start:
-        trd = Process(target=uvicorn.run,kwargs={"app":"main:app","port":5060,"host":"localhost"})
-        start = True
-        return trd
-
-def stop_fastapi(prcss:Process):
-    print('stopping the server')
-    prcss.terminate()
-
-        
+    return "pong"
 
 def mock_func(req:Request):
-    print(os.getpid())
-    global start
-    global fapi_prcss
-    if not start:
-        if req.headers.get('X-Start-Api','') == '1':
-            if req.authorization:
-                if req.authorization.token == 'authorize':
-                    fapi_prcss = start_fastapi()
-                    fapi_prcss.start()
-                    start = True
-                    return f"fast api running on Process {fapi_prcss.pid}",200
-    
-    if req.headers.get('X-STOP-API','') == '1':
-        if req.authorization:
-            if req.authorization.token == 'authorize':
-                if start:
-                    if fapi_prcss:
-                        stop_fastapi(fapi_prcss)
-                        start = False
-                        return f"fast api stopped"
+
+    if os.environ.get(API_STARTED_NAME) == '0':
+        api.start()
+        os.environ[API_STARTED_NAME] = '1'
     
     return {
         "args":req.args,
@@ -57,15 +29,13 @@ def mock_func(req:Request):
         "base_url":req.base_url,
         "blueprints":req.blueprints,
         "content_length":req.content_length,
-        "headers":{k:req.headers.get(k) for k in req.headers.keys()},
+        "headers":{k:v for k,v in req.headers.items()},
         "endpoint":req.endpoint,
         "is_json":req.is_json,
         "data":req.data.decode('utf-8'),
         "cookies":list(req.cookies.lists()),
         "environ":list(req.environ.keys()),
         'method' : req.method,
-        'query' : req.query_string.decode('utf-8')
+        'query' : req.query_string.decode('utf-8'),
+        'path' : req.path,
     },200
-
-
-
